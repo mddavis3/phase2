c00a993f954e8bb64db45fc193b8bd38ea604df5
mddavis3 <mdavis3@email.arizona.edu>
2020-02-24 23:01:23 -0700

    MboxCreate good, MboxSend begun, test00-02 good

--- message.h
+++ message.h
@@ -1,11 +1,17 @@
 #define DEBUG2 1
+#define ACTIVE 1              //added by Michael for MboxSend
+#define INACTIVE 0            //added by Michael for MboxSend
 
 typedef struct mail_slot *slot_ptr;
 typedef struct mailbox mail_box;
 typedef struct mbox_proc *mbox_proc_ptr;
 
 struct mailbox {
-   int           mbox_id;
+   int      is_free;          //added by Michael for MboxCreate
+   int      mbox_id;
+   int      status;           //added by Michael for MboxSend
+   int      num_slots;        //added by Michael for MboxCreate
+   int      max_slot_size;    //added by Michael for MboxCreate           
    /* other items as needed... */
 };
 
--- phase2.c
+++ phase2.c
@@ -23,6 +23,7 @@ extern int start2 (char *);
 static void enableInterrupts(void);    //added by Michael for test00
 void disableInterrupts(void);          //added by Michael for test00
 int check_io(void);                    //added by Michael for test00
+void check_kernel_mode(void);          //added by Michael for start1
 
 
 /* -------------------------- Globals ------------------------------------- */
@@ -32,6 +33,11 @@ int debugflag2 = 0;
 /* the mail boxes */
 mail_box MailBoxTable[MAXMBOX];
 
+/* Mail box id used to keep track of unique mail box ids */ //added by Michael for MboxCreate
+int global_mbox_id;
+
+/* Dummy mailbox used for initialization */                 //added by Michael for start1
+mail_box dummy_mbox = {0, NULL, NULL, NULL, NULL};
 
 /* -------------------------- Functions ----------------------------------- */
 
@@ -48,17 +54,32 @@ int start1(char *arg)
    int kid_pid, status;
 
    if (DEBUG2 && debugflag2)
+   {
       console("start1(): at beginning\n");
+   }
 
-   //check_kernel_mode("start1");
+   /* Check for kernel mode */
+   check_kernel_mode();
 
    /* Disable interrupts */
    disableInterrupts();
 
    /* Initialize the mail box table, slots, & other data structures.
-    * Initialize int_vec and sys_vec, allocate mailboxes for interrupt
-    * handlers.  Etc... */
+    * Initialize the Process Table
+    * Create I/O mailboxes and Initialize int_vec and sys_vec arrays, 
+    * allocate mailboxes for interrupt handlers.
+    */
 
+   /* Initialize global mbox_id to 0 */
+   global_mbox_id = 0;
+
+   /* Initialize the mail box table */
+   for (int i = 0; i < MAXMBOX; i++)
+   {
+      MailBoxTable[i] = dummy_mbox;
+   }
+
+   /* Enable interrupts */
    enableInterrupts();
 
    /* Create a process for start2, then block on a join until start2 quits */
@@ -69,7 +90,7 @@ int start1(char *arg)
 
    kid_pid = fork1("start2", start2, NULL, 4 * USLOSS_MIN_STACK, 1);
 
-   if ( join(&status) != kid_pid ) 
+   if (join(&status) != kid_pid) 
    {
       console("start2(): join returned something other than start2's pid\n");
    }
@@ -90,6 +111,47 @@ int start1(char *arg)
    ----------------------------------------------------------------------- */
 int MboxCreate(int slots, int slot_size)
 {
+   /* Check for kernel mode */
+   check_kernel_mode();
+
+   int i = 0;
+
+   //check for a free mailbox from the table of mailboxes
+   while (MailBoxTable[i].is_free != 0)
+   {
+      i++;
+      //return -1 if no mailboxes are available in mailboxtable
+      if (i >= MAXMBOX)
+      {
+         if (DEBUG2 && debugflag2)
+         {
+            console ("MboxCreate(): no mailboxes available. Return -1\n");
+         }
+         return (-1);
+      }
+   }
+
+   //return -1 if slot_size is incorrect
+   if (slot_size > MAX_MESSAGE)
+   {
+      if (DEBUG2 && debugflag2)
+      {
+         console ("MboxCreate(): slot_size is incorrect. Return -1\n");
+      }
+      return (-1);
+   }
+
+   //initialize the mailbox struct
+   MailBoxTable[i].is_free = 1;       //indicates that the mailboxtable slot is full
+   MailBoxTable[i].status = ACTIVE;   //indicates that the mailbox is active
+   MailBoxTable[i].num_slots = slots;
+   MailBoxTable[i].max_slot_size = slot_size;
+
+   //increment global int mbox_id
+   //return >= 0 as the mailbox id number
+   global_mbox_id++;
+   return global_mbox_id;
+
 } /* MboxCreate */
 
 
@@ -103,6 +165,48 @@ int MboxCreate(int slots, int slot_size)
    ----------------------------------------------------------------------- */
 int MboxSend(int mbox_id, void *msg_ptr, int msg_size)
 {
+   /* Check for kernel mode */
+   check_kernel_mode();
+
+   int i = 0;
+
+   //check for the mailbox in the mailbox table
+   while (MailBoxTable[i].mbox_id != mbox_id)
+   {
+      i++;
+      //return -1 if mailbox is not in the mailbox table
+      if (i == MAXMBOX)
+      {
+         if (DEBUG2 && debugflag2)
+         {
+            console ("MboxSend(): mailbox not found in the mailbox table. Return -1\n");
+         }
+         return (-1);
+      }
+   }
+
+   //return -1 if mailbox id is inactive
+   if (MailBoxTable[i].status == INACTIVE)
+   {
+      if (DEBUG2 && debugflag2)
+      {
+         console ("MboxSend(): mailbox id is inactive. Return -1\n");
+      }
+      return (-1);
+   }
+
+   //return -1 if message size is too large
+   if (msg_size > MAX_MESSAGE)
+   {
+      if (DEBUG2 && debugflag2)
+      {
+         console ("MboxSend(): message size is too large. Return -1\n");
+      }
+      return (-1);
+   }
+
+   //block the calling process until the message is sent
+
 } /* MboxSend */
 
 
@@ -117,6 +221,9 @@ int MboxSend(int mbox_id, void *msg_ptr, int msg_size)
    ----------------------------------------------------------------------- */
 int MboxReceive(int mbox_id, void *msg_ptr, int msg_size)
 {
+   /* Check for kernel mode */
+   check_kernel_mode();
+
 } /* MboxReceive */
 
 /* --------------------------------------------------------------------------------
@@ -158,3 +265,23 @@ int check_io()
 {
    return 0;
 } /* check_io */
+
+/* -------------------------------------------------------------------------
+   Name - check_kernel_mode()
+   Purpose - Checks the PSR to see what the current mode is. Halts(1) if
+             the current mode is user mode. Returns otherwise.
+   Parameters - None
+   Returns - Nothing
+   --------------------------------------------------------------------------*/
+void check_kernel_mode()
+{
+   // test if in kernel mode; halt if in user mode
+   if ((PSR_CURRENT_MODE & psr_get()) == 0)
+   {
+      console("check_kernel_mode(): Error - current mode is user mode. Halt(1)\n");
+      halt(1);
+   }
+
+   // return if in kernel mode
+   return;
+} /* check_kernel_mode */
